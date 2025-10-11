385c904dadca2fa8623e8d8c34d4b8cd1829e3d1
wisehero <godjw96@gmail.com>
2025-10-11 15:10:37 +0900

    ADD - root gradle에 롬복 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,25 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import org.gradle.kotlin.dsl.configure
+
+fun getGitHash(): String {
+    return runCatching {
+        providers.exec {
+            commandLine("git", "rev-parse", "--short", "HEAD")
+        }.standardOutput.asText.get().trim()
+    }.getOrElse { "init" }
+}
+
+
 plugins {
     java
     id("org.springframework.boot") version "3.5.6" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 allprojects {
@@ -18,14 +36,12 @@ subprojects {
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
 
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(21))
-        }
-    }
-
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter")
+
+        compileOnly("org.projectlombok:lombok")
+        annotationProcessor("org.projectlombok:lombok")
+
         testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
 

bdf51899873809be6ae8ab789685a60e80a19da0
wisehero <godjw96@gmail.com>
2025-10-11 15:10:19 +0900

    ADD - Caller에 Resilience4J 및 FeignClient 추가

--- caller/build.gradle.kts
+++ caller/build.gradle.kts
@@ -1,5 +1,18 @@
+val springCloudVersion by extra("2025.0.0")
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
+
 }
+
 
